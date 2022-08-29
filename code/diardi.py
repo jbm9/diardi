3d913e09af76804ab0f204ecbe095f7c91af1aa2
Josh Myer <josh@joshisanerd.com>
2022-08-29 11:01:55 -0700

    Bump to v0.0.2 due to new computational strategy
    I was getting a lot of silent dropout when deploying this to a toy
    machine.  To debug this, I needed a bit better debugging support.
    Going from strace, it seems like the sound device has just stopped
    responding, so I refactored the code to accumulate in the callbacks,
    then act on that in the main loop.
    
    This changes the way the data gets computed in a meaningful way, so
    I'm bumping the version.
    
    We can now identify when we haven't seen any samples, and after a few
    times of that, the process can exit and have systemd restart us up.

--- code/diardi.py
+++ code/diardi.py
@@ -11,10 +11,14 @@ from influxdb import InfluxDBClient
 import numpy as np
 import sounddevice as sd
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DEFAULT_BUCKET = f"soundlevel_v{VERSION}"
 
 TAGS = {"version": VERSION, "sensorId": 0}
+
+
+############################################################
+# Arg parsing
 
 def int_or_str(text):
     """Helper function for argument parsing."""
@@ -24,45 +28,57 @@ def int_or_str(text):
         return text
 
 
-# The following slightly weird parser config is stolen from a
-# sounddevice library example program
+parser = argparse.ArgumentParser(description=__doc__,
+                                 formatter_class=argparse.RawDescriptionHelpFormatter)
 
-parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument('-l', '--list-devices', action='store_true',
                     help='show list of audio devices and exit')
 
-args, remaining = parser.parse_known_args()
+parser.add_argument("--debug", action="store_true",
+                    help="Enable debug logging")
+
+parser.add_argument('-d', '--device', type=int_or_str,
+                    help='input device (numeric ID or substring)')
+
+parser.add_argument('-b', '--blocksize', type=int, metavar='NSAMPLES',
+                    default=1<<(11+3),
+                    help='block size (default: %(default)s samples)')
+
+parser.add_argument("-t", "--time", type=float, metavar="SECONDS",
+                    default=30,
+                    help="Time between sample transmissions (default: %(default)s seconds)")
+
+parser.add_argument("--server", default="127.0.0.1", metavar="HOSTNAME",
+                    help="Influxdb hostname string to use (default: %(default)s)")
+
+parser.add_argument("--database", default=DEFAULT_BUCKET,
+                    help=f"Influxdb database to use (default: %(default)s)")
+
+parser.add_argument("--measurement-name", default="fftmag",
+                    help="Name of measurement (default: %(default)s)")
+
+parser.add_argument("--nodename", default=platform.node(),
+                    help="Hostname to use when submitting data (default: %(default)s)")
+
+args = parser.parse_args()
+
+if args.debug:
+    logging.basicConfig(level=logging.DEBUG)
+
+logging.debug(f'Got args: {args}')
 
 if args.list_devices:
     print(sd.query_devices())
     parser.exit(0)
 
-parser = argparse.ArgumentParser(description=__doc__,
-                                 formatter_class=argparse.RawDescriptionHelpFormatter,
-                                 parents=[parser])
+############################################################
+# Actual program
 
-parser.add_argument('-d', '--device', type=int_or_str,
-                    help='input device (numeric ID or substring)')
-
-parser.add_argument('-b', '--blocksize', type=int, metavar='NSAMPLES', default=1<<20,
-                    help='block size (default %(default)s samples)')
-
-parser.add_argument("--server", default="127.0.0.1", metavar="HOSTNAME",
-                    help="Influxdb hostname string to use (default: %(default)s)")
-parser.add_argument("--database", default=DEFAULT_BUCKET,
-                    help=f"Influxdb database to use (default: %(default)s)")
-parser.add_argument("--measurement-name", help="Name of measurement (default: %(default)s)", default="fftmag")
-
-parser.add_argument("--nodename", default=platform.node(),
-                    help="Hostname to use when submitting data (default: %(default)s)")
-
-args = parser.parse_args(remaining)
 samplerate = 48000
 
 fftsize = 2048
 
 TAGS["nodename"] = args.nodename
-
 
 client = InfluxDBClient(host=args.server)
 client.create_database(args.database)
@@ -75,37 +91,26 @@ client.write_points([{
             }])
 
 
+####################
+# Global shared: note that this confines us to single-threading without a mutex
+bin_accumulator = np.zeros(fftsize)  # Accumulate squares of bins
+n_samples = 0  # Count of FFT samples we've taken
+
+# A queue of readings to kick over to the server, used to queue up
+# status messages for OOB sending.
+frame_queue = []
+
 def callback(indata, frames, time, status):
-    # A handy A-weighting IIR filter for 48kHz by Matt L, from:
-    # https://dsp.stackexchange.com/questions/36077/design-of-a-digital-a-weighting-filter-with-arbitrary-sample-rate
+    '''Callback for sounddevice after it's accumulated our frames
 
-    b =[ 0.169994948147430,
-         0.280415310498794,
-         -1.120574766348363,
-         0.131562559965936,
-         0.974153561246036,
-         -0.282740857326553,
-         -0.152810756202003,]
-
-
-    a = [ 1.00000000000000000,
-          -2.12979364760736134,
-          0.42996125885751674,
-          1.62132698199721426,
-          -0.96669962900852902,
-          0.00121015844426781,
-          0.04400300696788968,
-          ]
+    This just accumulates state into the above globals, with all
+    influx work happening in the main loop below.
+    '''
+    global bin_accumulator, n_samples, frame_queue
 
     ts = datetime.datetime.utcnow().isoformat()
-    json_body = []
 
-    client.write_points([{
-            "measurement": "foobar",
-            "fields": { "i": 1, },
-            "time": ts,
-            }])
-
+    #    logging.debug(f'Got samples {len(indata)}')
     got_status = False
 
     if status:
@@ -113,7 +118,7 @@ def callback(indata, frames, time, status):
         errtags = TAGS.copy()
         errtags["error"] = "got_status"
 
-        json_body.append({
+        frame_queue.append({
             "measurement": args.measurement_name + "_error",
             "fields": { "status_text": status, },
             "tags": errtags,
@@ -124,56 +129,14 @@ def callback(indata, frames, time, status):
 
     if any(indata):
         n0 = 0
-        hz_per_bin = samplerate/fftsize
-
-        f0 = 0
-        f_nyquist = samplerate//2  # integer for formatting
-
-        splits = [ 100, 500, 1000, 2500, 5000, 10000, ]
-        mag_sums = [ 0 for _ in splits ]
-        mag_cnts = [ 0 for _ in splits ]
 
         while n0 < indata.shape[0]-fftsize:
-            mags = np.abs(np.fft.rfft(indata[n0:n0+fftsize,0], n=fftsize))
+            mags = np.abs(np.fft.rfft(indata[n0:n0+2*fftsize, 0]))
             mags *= mags
+
+            bin_accumulator += mags[:-1]  # drop the nyquist bin
+            n_samples += 1
             n0 += fftsize
-
-            last_bin1 = 1 # skip DC
-            for i, f_cut in enumerate(splits):
-                if f_cut > f_nyquist:
-                    break
-
-                bin0 = last_bin1
-                bin1 = min(f_cut//hz_per_bin, fftsize)
-
-                for j in range(int(bin0), int(bin1)):
-                    mag_sums[i] += mags[j]
-                    mag_cnts[i] += 1
-
-                last_bin1 = bin1
-
-
-        results = { "rms": np.sqrt(np.sum(mags)/len(mags))}
-        last_fcut = 0
-        for i, f_cut in enumerate(splits):
-            if f_cut > f_nyquist:
-                break
-
-            lbl = f"f{last_fcut}-{f_cut}"
-            results[lbl] = np.sqrt(mag_sums[i]/mag_cnts[i])
-            last_fcut = f_cut
-
-        d = {
-            "measurement": args.measurement_name,
-            "tags": TAGS,
-            "time": ts,
-            "fields": results,
-        }
-
-        json_body.append(d)
-
-
-
     else:
         logging.warning("No data received in callback")
 
@@ -182,23 +145,100 @@ def callback(indata, frames, time, status):
             errtags = TAGS.copy()
             errtags["error"] = "got_status"
 
-            json_body.append({
+            frame_queue.append({
                 "measurement": args.measurement_name + "_error",
                 "fields": { "status_text": "Empty data" },
                 "time": ts,
             })
 
-    client.write_points(json_body)
 
 try:
     with sd.InputStream(device=args.device, channels=1, callback=callback,
                         blocksize=args.blocksize, samplerate=samplerate):
 
-        while True:
-            time.sleep(1)
+        n_zero_loops = 0
+        max_zero_loops = 3
 
+        while True:
+            time.sleep(args.time)
+
+            splits = [ 100, 500, 1000, 2500, 5000, 10000, ]
+            mag_sums = np.zeros_like(splits)
+            mag_cnts = np.zeros_like(mag_sums)
+
+            # Send the backlog of error messages if we have one
+            if frame_queue:
+                logging.debug(f'Submitting {len(frame_queue)} errors')
+                client.write_points(frame_queue)
+                frame_queue.clear()
+
+            # Record the overall RMS, and number of samples
+            results = {
+                "rms": np.sqrt(np.sum(bin_accumulator)/len(bin_accumulator)/n_samples),
+                "n_samples": n_samples,
+            }
+
+            # Compute the aggregated frequency range values
+            hz_per_bin = samplerate/fftsize
+            f_nyquist = samplerate//2  # integer for formatting
+            last_bin1 = 1  # skip DC
+            for i, f_cut in enumerate(splits):
+                if f_cut > f_nyquist:
+                    break
+
+                bin0 = last_bin1
+                bin1 = int(min(f_cut//hz_per_bin, fftsize))
+
+                mag_sums[i] += np.sum(bin_accumulator[bin0:bin1])
+                mag_cnts[i] = bin1 - bin0
+
+                last_bin1 = bin1
+
+            if not n_samples:
+                n_zero_loops += 1
+                if n_zero_loops >= max_zero_loops:
+                    logging.error(f'Got {n_zero_loops} loops without any samples recorded, bailing')
+                    res_body = [{
+                        "measurement": args.measurement_name + "_error",
+                        "fields": { "status_text": "Too many zero samples",
+                                    "n_zero_loops": n_zero_loops, },
+                        "time":  datetime.datetime.utcnow().isoformat()
+                    }]
+                    client.write_points(res_body)
+
+                    parser.exit(f'Exceeded max zero sample loops, exiting to restart sound device')
+            else:
+                # Generate our per-range results
+                last_fcut = 0
+                for i, f_cut in enumerate(splits):
+                    if f_cut > f_nyquist:
+                        break
+
+                    lbl = f"f{last_fcut}-{f_cut}"
+                    results[lbl] = np.sqrt(mag_sums[i]/mag_cnts[i]/n_samples)
+                    last_fcut = f_cut
+
+            n_samples = 0
+            bin_accumulator = np.zeros_like(bin_accumulator)
+
+            # And bundle it all up to send
+            ts = datetime.datetime.utcnow().isoformat()
+            d = {
+                "measurement": args.measurement_name,
+                "tags": TAGS,
+                "time": ts,
+                "fields": results,
+            }
+
+            logging.debug(d)
+
+            json_body = [d]
+            client.write_points(json_body)
+            logging.debug(f'Sent datapoints: {json_body}')
 
 except KeyboardInterrupt:
+    logging.error('Got keybord interrupt')
     parser.exit('Interrupted')
 except Exception as e:
+    logging.error(f'Got exception: {e}')
     parser.exit(type(e).__name__ + ": " + str(e))
